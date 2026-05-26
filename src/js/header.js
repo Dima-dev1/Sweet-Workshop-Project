f011538cfb41b6bffb3167d0495d71e84130fe98
Захар <zohanimaa@gmail.com>
2026-05-26 13:23:35 +0300

    fix

--- src/js/header.js
+++ src/js/header.js
@@ -1,2 +1,36 @@
+const burgerBtn = document.querySelector('.burger-btn');
+const mobileMenu = document.querySelector('.mobile-menu');
+const closeBtn = document.querySelector('.mobile-menu-close');
+const menuLinks = document.querySelectorAll('.mobile-menu-link');
+const shopBtn = document.querySelector('.btn-link');
+
+burgerBtn.addEventListener('click', () => {
+  mobileMenu.classList.add('is-open');
+  document.body.style.overflow = 'hidden';
+});
+
+closeBtn.addEventListener('click', () => {
+  mobileMenu.classList.remove('is-open');
+  document.body.style.overflow = '';
+});
+
+menuLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    mobileMenu.classList.remove('is-open');
+    document.body.style.overflow = '';
+  });
+});
+
+shopBtn.addEventListener('click', () => {
+  mobileMenu.classList.remove('is-open');
+  document.body.style.overflow = '';
+});
+
+document.addEventListener('keydown', e => {
+  if (e.key === 'Escape' && mobileMenu.classList.contains('is-open')) {
+    mobileMenu.classList.remove('is-open');
+    document.body.style.overflow = '';
+  }
+});
 
 
