922829b47b1e7f2bf953089b2f142601aa52c4d4
Mykhailo <mishka_19@icloud.com>
2026-05-27 20:52:08 +0200

    correcting section

--- src/js/dessertList.js
+++ src/js/dessertList.js
@@ -76,7 +76,7 @@ async function loadDesserts(categoryId = null, isLoadMore = false) {
                 <p class="description-dessert">${description}</p>
                 
                 <div class="dessert-card-footer">
-                    <p class="dessert-price">Ціна: <span>${price} грн</span></p>
+                    <p class="dessert-price">${price} грн</p>
                     
                     <button type="button" class="open-modal-btn" data-id="${_id}">
                      <svg class="modal-btn-svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
